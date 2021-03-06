1561d2c5a529d834c3824ed1e027ddf89251a4d1
ngmicroapp <ngmicroapp@gmail.com>
2021-03-06 12:36:24 +0530

    inital commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const init = () => {
+    return () => {
+        const container = document.getElementById("grid-container");
+        const shuffle = (items) => {
+            let temp, randomNumber;
+            for (let index = items.length - 1; index > 0; index--) {
+                randomNumber = Math.floor((index + 1) * Math.random());
+                temp = items[randomNumber];
+                items[randomNumber] = items[index];
+                items[index] = temp;
+            }
+            return items;
+        }
+        const sort = (items) => {
+            items.sort((a, b) => a.firstChild.innerHTML - b.firstChild.innerHTML);
+            return items;
+        }
+        const shuffleTheList = () => {
+            for (const grid of shuffle([...container.children])) {
+                container.append(grid);
+            }
+        }
+        const sortTheList = () => {
+            for (const grid of sort([...container.children])) {
+                container.append(grid);
+            }
+        }
+        document.getElementById("shuffle-btn").onclick = shuffleTheList;
+        document.getElementById("sort-btn").onclick = sortTheList;
+    }
+};
+window.onload = init();
